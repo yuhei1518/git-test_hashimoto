5c42c3cc19c1a8117abddbbd0faf32082236c1e2
segit1972 <g.chatkazubokkuri@gmail.com>
2024-03-14 15:36:18 +0900

    first

--- kadai.php
+++ kadai.php
@@ -20,11 +20,12 @@ if (!empty($_POST)) {
     if (empty($error)) {
         try {
 
-            $statement = $pdo->prepare('INSERT INTO comments (name, mail, coment) VALUES (:name, :mail, :coment)');
+            $statement = $pdo->prepare('INSERT INTO comments (name, mail, coment, subject) VALUES (:name, :mail, :coment, subject)');
             $statement->execute(array(
                 ':name' => $_POST['name'],
                 ':mail' => $_POST['mail'],
                 ':coment' => $_POST['coment'],
+                ':subject' => $_POST['subject'],
             ));
 
             header('Location: kadai.php');
@@ -62,6 +63,23 @@ if (!empty($_POST)) {
         </div>
     </div>
 
+        <div class="segi1">
+          <ul><br>
+            <li><strong>名前:</strong> 瀬木　和憲</li>
+            <li><strong>生年月日:</strong> 1972年5月23日</li>
+            <li><strong>住所:</strong> 三重県松阪市</li>
+            <li><strong>好きなもの:</strong>サッカー観戦・ドライブ・スズメ（小鳥）</li><br>
+          </ul>
+        </div>
+        <div class="segi2">自己紹介<br>
+          <p>もともとプロセス（工法）開発系のお仕事をしてました。<br>
+            複雑な製品になると担当分野が狭くなりがちなので、<br>
+            自分で全体をみれるような開発業を探してきました。<br>
+            今は自分でコードを完成できるようにしたいです。<br>
+          </p>
+        </div>
+        </div>
+
     <div class="from">
         <form action="" method="post">
             ユーザー名：<br />
@@ -79,6 +97,11 @@ if (!empty($_POST)) {
             <?php if (isset($error['coment']) && ($error['coment'] == "blank")) : ?>
                 <p class="error">*コメントくれへんなんて寂しいわ～</p>
             <?php endif; ?>
+            宛先：<br />
+            <select name="subject" id="recipient">
+                <option value="hashimoto">橋本</option>
+                <option value="segi">瀬木</option>
+            </select><br><br>
             <input type="submit" value="登録する" />
         </form>
 
