bb4bdb29eaeec20d695af9eebd0a4e50de6b9cdb
yuhei1518 <yuhei23491518@gmail.com>
2024-03-14 12:50:50 +0900

    これもみてね

---
+++ date.php
@@ -0,0 +1,7 @@
+<?php
+try {
+    $pdo = new PDO("mysql:host=localhost; dbname=git-test; charset=utf8", "root", "");
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo 'DB接続エラー' . $e->getMessage();
+}
